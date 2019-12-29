bd87ee329610d4bc30fe4672df5ae0b56e8537f5
daviti khomasuridze <davidxoma@gmail.com>
2019-12-29 03:40:08 -0800

    test version

--- gen.php
+++ gen.php
@@ -9,11 +9,33 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <form action="gen.php" method="post">
-        <input type="text" name="sname">
-        <input type="submit" value="შექმენი">
-    </form>
+<?php
+
+
+header('contont-type: image/jpeg');
+$user = $_POST['sname'];
+$source = 'images/bg.jpg';
+$img1 = imagecreatefromjpeg($source);
+$output = 'images/bgs.jpg';
+
+//Parameters
+$font = __DIR__ .'\fonts\3d_unicode.ttf';
+$color = imagecolorallocate($img1, 0,0,0);
+$x = 240;
+$y = 750;
+
+imagettftext($img1, 50,0,$x, $y, $color, $font, $user);
+imagejpeg($img1, $output, 99);
+
+
+?>
+<div class="container">
+<div class="row justify-content-center">
+<img class="img-fluid created-img" src="<?php echo $output; ?>" alt="">
+</div>
+</div>
 </body>
 </html>
